e1fc1345c4457367d6f8a7c3164112d6dd02cfde
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-04-02 16:35:59 +0530

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 
+# Specify the latest version of JFrog/Artifactory below. You can get the direct link here: https://jfrog.com/community/download-artifactory-oss/
+VERSION=7.133.17
 
-# Installing necessary packages
+path=$(pwd)
 
-sudo apt-get update -y > /dev/null 2>&1
-sudo apt-get install -y openjdk-11-jre unzip > /dev/null 2>&1
+# Identifying the distro of the server.
+distro=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f2 | sed 's/"//g')
 
+echo
+echo
+echo
+echo "Installing JFrog/Artifactory on $distro"
 
-# Configuring Artifactory as a Service
-sudo useradd -r -m -U -d /opt/artifactory -s /bin/false artifactory 2>/dev/null
-sudo cp artifactory.service /etc/systemd/system/artifactory.service
-sudo systemctl daemon-reload 1>/dev/null
+if [ "$distro" == "rhel" ]; then
 
+    sudo yum update -y > /dev/null 2>&1
+    sudo yum upgrade -y > /dev/null 2>&1
+    sudo yum install wget -y > /dev/null 2>&1
+    sudo dnf install java-25-openjdk-devel -y > /dev/null 2>&1
 
-# Downloading JFROG Artifactory 6.9.6 version to OPT folder
-echo "Downloading JFROG Artifactory 6.9.6 version"
-sudo systemctl stop artifactory > /dev/null 2>&1
-cd /opt 
-sudo rm -rf jfrog* artifactory*
-sudo wget -q https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
-sudo unzip -q jfrog-artifactory-oss-6.9.6.zip -d /opt/artifactory 1>/dev/null
-sudo chown -R artifactory: /opt/artifactory/*
-sudo rm -rf jfrog-artifactory-oss-6.9.6.zip
+elif [ "$distro" == "ubuntu" ]; then
 
+    sudo apt-get update -y > /dev/null 2>&1
+    sudo apt-get upgrade -y > /dev/null 2>&1
+    sudo apt-get install openjdk-25-jdk -y > /dev/null 2>&1
 
-# Starting Artifactory Service
-
-sudo systemctl start artifactory 1>/dev/null
-
-
-# Check if Artifactory is working
-sudo systemctl is-active --quiet artifactory
-echo -e "\n################################################################ \n"
-if [ $? -eq 0 ]; then
-	echo "Artifactory installed Successfully"
-	echo "Access Artifactory using $(curl -s ifconfig.me):8081"
 else
-	echo "Artifactory installation failed"
+    echo "Unsupported Distribution - Only RHEL and Ubuntu are supported by this Script!!!!"
+    exit 1
 fi
-echo -e "\n################################################################ \n"
+
+
+wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/$VERSION/jfrog-artifactory-oss-$VERSION-linux.tar.gz > /dev/null 2>&1
+
+sudo mkdir -p /opt/jfrog
+sudo tar -xzf jfrog-artifactory-oss-$VERSION-linux.tar.gz -C /opt/jfrog > /dev/null 2>&1
+
+cd /opt/jfrog
+sudo mv artifactory-oss-* artifactory
+
+sudo useradd -r -m -U -d /opt/jfrog/artifactory -s /bin/bash artifactory
+
+sudo chown -R artifactory:artifactory /opt/jfrog
+
+sudo cp $path/artifactory.service /etc/systemd/system/artifactory.service
+
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+
+sudo systemctl enable artifactory
+sudo systemctl start artifactory > /dev/null 2>&1
+
+echo "JFrog/Artifactory installed the $distro successfully."
+echo "You can access with the URL: http://$(curl -s ifconfig.me):8082\n"
+echo -e "\n\n\nYou can check the port here: /opt/jfrog/artifactory/var/etc/system.yaml  --> externalPort: 8082"
