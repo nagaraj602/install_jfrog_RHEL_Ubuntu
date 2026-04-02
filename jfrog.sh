353cfa1926b5f886f36c84f540f62cd033975943
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-04-02 14:49:55 +0530

    Create jfrog.sh

---
+++ jfrog.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Installing Jfrog Artifactory on Ubuntu"
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get install -y openjdk-21-jre unzip > /dev/null 2>&1
+
+
+# Configuring Artifactory as a Service
+sudo useradd -r -m -U -d /opt/artifactory -s /bin/false artifactory 2>/dev/null
+sudo cp artifactory.service /etc/systemd/system/artifactory.service
+sudo systemctl daemon-reload 1>/dev/null
+
+
+# Downloading JFROG Artifactory 6.9.6 version to OPT folder
+echo "*****Downloading JFROG Artifactory 6.9.6 version"
+sudo systemctl stop artifactory > /dev/null 2>&1
+cd /opt 
+sudo rm -rf jfrog* artifactory*
+sudo wget -q https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
+sudo unzip -q jfrog-artifactory-oss-6.9.6.zip -d /opt/artifactory 1>/dev/null
+sudo chown -R artifactory: /opt/artifactory/*
+sudo rm -rf jfrog-artifactory-oss-6.9.6.zip
+
+
+# Starting Artifactory Service
+sudo systemctl start artifactory 1>/dev/null
+
+
+# Check if Artifactory is working
+sudo systemctl is-active --quiet artifactory
+echo "\n################################################################ \n"
+if [ $? -eq 0 ]; then
+	echo "Artifactory installed Successfully"
+	echo "Access Artifactory using $(curl -s ifconfig.me):8081"
+else
+	echo "Artifactory installation failed"
+fi
+echo "\n################################################################ \n"
